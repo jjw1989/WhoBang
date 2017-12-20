9e6ebeca702feedee689df3101326511e40efab2
sundy <sundy.jiang@powervision.me>
2017-12-21 01:17:48 +0800

    服务标签

--- app/src/main/java/com/whombang/app/features/mycenter/activity/AddServerActivity.java
+++ app/src/main/java/com/whombang/app/features/mycenter/activity/AddServerActivity.java
@@ -1,6 +1,7 @@
 package com.whombang.app.features.mycenter.activity;
 
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -11,8 +12,11 @@ import com.whombang.app.common.base.BaseActivity;
 import com.whombang.app.common.net.EasyHttp;
 import com.whombang.app.common.net.callback.SimpleCallBack;
 import com.whombang.app.common.net.exception.ApiException;
+import com.whombang.app.common.tabGround.OneTagLabel;
 import com.whombang.app.common.tabGround.TagBean;
 import com.whombang.app.common.tabGround.TagContainerLayout;
+import com.whombang.app.common.tabGround.TagFactory;
+import com.whombang.app.common.tabGround.TagView;
 import com.whombang.app.common.view.TitleBar;
 import com.whombang.app.entity.UserLocalData;
 
@@ -22,6 +26,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
 
 import butterknife.BindView;
 import io.reactivex.Flowable;
@@ -38,32 +43,32 @@ public class AddServerActivity extends BaseActivity {
      */
     private List<TagBean> mTagBean;
     @BindView(R.id.tag_service_type)
-    TagContainerLayout  tagSetviceType;
+    TagContainerLayout tagSetviceType;
+    private TagContainerLayout.ViewColor mBanViewColor;
+    private TagContainerLayout.ViewColor mDefaultViewColor;
+    private TagContainerLayout.ViewColor mClickViewColor;
+    private TagFactory tagFactory;
+
     @Override
     public void initData(Bundle bundle) {
-         initTagData();
+        mBanViewColor = new TagContainerLayout.ViewColor();
+        mDefaultViewColor = new TagContainerLayout.ViewColor(ContextCompat.getColor(this, R.color.backGroundColor), 0, ContextCompat.getColor(this, R.color.textColor));
+        mClickViewColor = new TagContainerLayout.ViewColor(ContextCompat.getColor(this, R.color.clickBackGroundColor), 0, ContextCompat.getColor(this, R.color.clickTextColor));
+
+        initTagData();
     }
 
     /**
      * 初始化标签数据
      */
     private void initTagData() {
-       mTagBean=new ArrayList<>();
-       TagBean tagBean1=new TagBean();
-       tagBean1.setTitle("家政");
-       TagBean tagBean2=new TagBean();
-       tagBean2.setTitle("保洁");
-       TagBean tagBean3=new TagBean();
-       tagBean3.setTitle("法律咨询");
-       TagBean tagBean4=new TagBean();
-       tagBean4.setTitle("维修");
-       TagBean tagBean5=new TagBean();
-       tagBean5.setTitle("其他");
-       mTagBean.add(tagBean1);
-       mTagBean.add(tagBean2);
-       mTagBean.add(tagBean3);
-       mTagBean.add(tagBean4);
-       mTagBean.add(tagBean5);
+        mTagBean = new ArrayList<>();
+
+        mTagBean.add(new TagBean("家政", 222, 1000));
+        mTagBean.add(new TagBean("保洁", 10, 1));
+        mTagBean.add(new TagBean("法律咨询", 20, 555));
+        mTagBean.add(new TagBean("维修", 30, 1));
+        mTagBean.add(new TagBean("其他", 40, 300));
     }
 
     @Override
@@ -92,7 +97,28 @@ public class AddServerActivity extends BaseActivity {
      * 初始化标签控件
      */
     private void initTagView() {
+        List<String> titles = new ArrayList<String>();
+        for (TagBean tagBean : mTagBean) {
+            titles.add(tagBean.getTitle());
+        }
+        tagSetviceType.setTitles(titles);
+        tagFactory = new OneTagLabel(mTagBean, tagSetviceType.getAllChildViews(), mBanViewColor, mDefaultViewColor, mClickViewColor);
+        tagSetviceType.setOnTagClickListener(new TagView.OnTagClickListener() {
+            @Override
+            public void onTagClick(TagView view, int position, String text) {
+                tagFactory.onColorTagClick(position);
+            }
 
+            @Override
+            public void onTagLongClick(int position, String text) {
+
+            }
+
+            @Override
+            public void onTagCrossClick(int position) {
+
+            }
+        });
     }
 
     /**
@@ -117,12 +143,12 @@ public class AddServerActivity extends BaseActivity {
 
                     @Override
                     public void onError(ApiException e) {
-                        Toast.makeText(mContext,e.getMessage(),Toast.LENGTH_SHORT).show();
+                        Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
                     public void onSuccess(String entity) {
-                        Log.i("www","data="+entity);
+                        Log.i("www", "data=" + entity);
                     }
                 });
     }
