90a06a2fbb41148b75a865b9d8b28c50ae6b1f92
蒋建伟 <sundy.jiang@irootech.com>
2017-12-27 21:33:59 +0800

    反馈

--- app/src/main/java/com/whombang/app/features/mycenter/activity/FeedbackActivity.java
+++ app/src/main/java/com/whombang/app/features/mycenter/activity/FeedbackActivity.java
@@ -1,19 +1,37 @@
 package com.whombang.app.features.mycenter.activity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.whombang.app.R;
 import com.whombang.app.common.base.BaseActivity;
+import com.whombang.app.common.net.EasyHttp;
+import com.whombang.app.common.net.callback.SimpleCallBack;
+import com.whombang.app.common.net.exception.ApiException;
 import com.whombang.app.common.view.TitleBar;
+import com.whombang.app.entity.UserInfoEntity;
+import com.whombang.app.entity.UserLocalData;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.BindView;
 
 /**
  * 反馈
  */
 @Route(path = "/set/feedback")
 public class FeedbackActivity extends BaseActivity{
-
+@BindView(R.id.et_feedback)
+    EditText etFeedBack;
     @Override
     protected int bindLayout() {
         return R.layout.wb_feedback_layout;
@@ -35,7 +53,11 @@ public class FeedbackActivity extends BaseActivity{
             titleBar.addAction(new TitleBar.TextAction("发送") {
                 @Override
                 public void performAction(View view) {
-
+                    if (!TextUtils.isEmpty(etFeedBack.getText().toString())) {
+                        sendFeedback();
+                    }else{
+                        Toast.makeText(mContext,"内容不能为空",Toast.LENGTH_SHORT).show();
+                    }
                 }
             });
     }
@@ -44,4 +66,28 @@ public class FeedbackActivity extends BaseActivity{
     public void doBusiness() {
 
     }
+
+    private void sendFeedback() {
+        final Map<String, Object> params = new HashMap<>();
+        params.put("userId", UserLocalData.getUserInfo(mContext).getUserInfo().getUserId());
+        params.put("feedbackContent", etFeedBack.getText().toString());
+
+        EasyHttp.post("userFeedback")
+                .upJson(new JSONObject(params).toString())
+                .execute(new SimpleCallBack<String>() {
+
+                    @Override
+                    public void onError(ApiException e) {
+                        Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onSuccess(String entity) {
+                        finish();
+                        Log.d("wwwww", "onSuccess: ");
+                    }
+                });
+    }
+
 }
