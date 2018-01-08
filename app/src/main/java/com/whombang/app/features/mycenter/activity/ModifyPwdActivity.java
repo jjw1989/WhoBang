75cbc4aa47ce8da4c7178cabde760ff3795a626c
sundy <sundy.jiang@powervision.me>
2018-01-08 22:42:04 +0800

    修改密码

--- app/src/main/java/com/whombang/app/features/mycenter/activity/ModifyPwdActivity.java
+++ app/src/main/java/com/whombang/app/features/mycenter/activity/ModifyPwdActivity.java
@@ -2,17 +2,23 @@ package com.whombang.app.features.mycenter.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.whombang.app.R;
 import com.whombang.app.common.base.BaseActivity;
+import com.whombang.app.common.view.TitleBar;
+import com.whombang.app.entity.UserLocalData;
+
+import butterknife.BindView;
 
 /**
  * 修改密码
  */
 @Route(path = "/set/modifypwd")
 public class ModifyPwdActivity extends BaseActivity {
-
+    @BindView(R.id.tv_modify_pwd)
+    TextView tvModifyPwd;
     @Override
     protected int bindLayout() {
         return R.layout.wb_modify_pwd_layout;
@@ -31,6 +37,16 @@ public class ModifyPwdActivity extends BaseActivity {
     @Override
     public void initView(Bundle savedInstanceState, View view) {
         titleBar.setTitle("修改密码");
+        titleBar.setLeftText("取消");
+        titleBar.addAction(new TitleBar.TextAction("完成") {
+            @Override
+            public void performAction(View view) {
+
+
+                finish();
+            }
+        });
+        tvModifyPwd.setText(UserLocalData.getUserInfo(this).getUserInfo().getUserTel());
     }
 
     @Override
